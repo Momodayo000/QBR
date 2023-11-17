3b4a92ea87d434bfad8ed5d232c661f6cb490953
EC2 Default User <ec2-user@ip-172-31-34-225.ap-northeast-1.compute.internal>
2023-11-17 06:21:45 +0000

    routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_admin!, if: :admin_url
+    before_action :authenticate_customer!, except: [:top], unless: :admin_url
+
+  def admin_url
+    request.fullpath.include?("/admin")
+  end
 end
