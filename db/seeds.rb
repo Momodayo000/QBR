694af91de93ffa60225e978de7711bb849500ff3
EC2 Default User <ec2-user@ip-172-31-34-225.ap-northeast-1.compute.internal>
2023-11-20 02:25:08 +0000

    db:seed add

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Menu.create!(
-  menu_title: "カットのみ",
-  menu_sub_title: "cut only",
-  menu_explanation: "カットだけだよ",
-  price: "1200~"
-  )
+require 'dotenv/load'
 
-Admin.create!(
-  email: "sample@gmail.com",
-  password: "adminadmin"
-  )
+Admin.find_or_create_by!(email: "sample@gmail.com") do |admin|
+  admin.password = ENV['ADMIN_PASSWORD']
+  puts "Adminのパスワードを設定しました。"
+end
+
+puts admin.inspect
